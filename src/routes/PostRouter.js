1fbf8303cf723e1efac033cdbd23fb8ac46cc154
John Hughes <jfh4193@g.rit.edu>
2017-12-08 22:52:57 -0800

    added ability to edit posts, retrieve a single post, and retrieve all posts from a user

--- src/routes/PostRouter.js
+++ src/routes/PostRouter.js
@@ -52,7 +52,53 @@ export default class PostRouter {
     init(): void {
         this.router.post('/create', this.createPost);
         this.router.delete('/delete/:id', this.deletePost);
+        this.router.post('/edit/:id', this.editPost);
+        this.router.get('/:id', this.getPost);
+        this.router.get('/userposts/:user_id', this.getUserPosts);
     }
+
+    getPost(req: $Request, res: $Response): void {
+        const postId = req.params.id;
+        const errorMsg : string = 'Unable to find post.';
+
+        PostModels.postDb.findOne({
+            where: {
+                id: postId,
+            }
+        }).then((data) => {
+            if (!!data) {
+                return res.status(200).json({
+                    data
+                });
+            } else {
+                return res.status(404).json({
+                    message: 'Specified post does not exist.'
+                });
+            }
+        }).catch((err) => {
+            logger.error(errorMsg, err, err.message);
+            return res.status(500).json({message: errorMsg});
+        });
+    }
+
+    getUserPosts(req: $Request, res: $Response): void {
+        const userId = req.params.user_id;
+        const errorMsg : string = 'Unable to find posts.';
+
+        PostModels.postDb.findAll({
+            where: {
+                submitter_user_id: userId,
+            }
+        }).then((data) => {
+            return res.status(200).json({
+                data
+            });
+        }).catch((err) => {
+            logger.error(errorMsg, err, err.message);
+            return res.status(500).json({message: errorMsg});
+        });
+    }
+
 
     deletePost(req: $Request, res: $Response): void {
         const postId = req.params.id;
@@ -123,35 +169,114 @@ export default class PostRouter {
             });
         }
 
-        PostModels.postDb.create({
-            id: params.id,
-            name: params.name,
-            last_seen: params.lastSeen,
-            reward: params.reward,
-            longitude: params.longitude,
-            latitude: params.latitude,
-            contact: params.contact,
-            description: params.description,
-            animal: params.animal,
-            breed: params.breed,
-            is_aggressive: params.isAggressive,
-            completed_shots: params.completedShots,
-            has_chip: params.hasChip,
-            found: params.found,
-            found_user_id: params.foundUserId,
-            deleted: params.deleted,
-            submitter_user_id: params.submitterUserId,
-        }).then((data) => {
-            return res.status(200).json({
-                message: 'Post created.',
-            });
-        }).catch((err) => {
-            logger.error(errorMsg, err, err.message);
+        if (!!req.session.key) {
+            PostModels.postDb.create({
+                id: params.id,
+                name: params.name,
+                last_seen: params.lastSeen,
+                reward: params.reward,
+                longitude: params.longitude,
+                latitude: params.latitude,
+                contact: params.contact,
+                description: params.description,
+                animal: params.animal,
+                breed: params.breed,
+                is_aggressive: params.isAggressive,
+                completed_shots: params.completedShots,
+                has_chip: params.hasChip,
+                found: params.found,
+                found_user_id: params.foundUserId,
+                deleted: params.deleted,
+                submitter_user_id: params.submitterUserId,
+            }).then((data) => {
+                return res.status(200).json({
+                    message: 'Post created.',
+                });
+            }).catch((err) => {
+                logger.error(errorMsg, err, err.message);
+                return res.status(400).json({
+                    message: errorMsg,
+                    error: err.message,
+                });
+            })
+        } else {
+            return res.status(500).json({
+                message: 'User not authenticated.'
+            })
+        }
+
+    }
+
+    editPost(req: $Request, res: $Response): void {
+        const postId = req.params.id;
+        const { body } = req;
+        const params : UpdatePostParams = {
+            name: body.name,
+            last_aeen: body.lastSeen,
+            reward: body.reward,
+            longitude: body.longitude,
+            latitude: body.latitude,
+            contact: body.contact,
+            description: body.description,
+            animal: body.animal,
+            breed: body.breed,
+            isAggressive: body.isAggressive,
+            completedShots: body.completedShots,
+            hasChip: body.hasChip,
+        };
+
+        let errorMsg : string = 'Unable to edit post.';
+
+        if (params.longitude < -180 || params.longitude > 180 || params.latitude < -90 || params.latitude > 90) {
             return res.status(400).json({
-                message: errorMsg,
-                error: err.message,
+                message: 'Invalid longitude-latitude combination.',
             });
-        })
+        }
+
+        if (!!req.session.key) {
+            PostModels.postDb.update({
+                name: body.name,
+                last_seen: body.lastSeen,
+                reward: body.reward,
+                longitude: body.longitude,
+                latitude: body.latitude,
+                contact: body.contact,
+                description: body.description,
+                animal: body.animal,
+                breed: body.breed,
+                is_aggressive: body.isAggressive,
+                completed_shots: body.completedShots,
+                has_chip: body.hasChip,
+            },{ 
+                where: {
+                    id: postId,
+                    submitter_user_id: req.session.key['id'],
+                },
+                returns: true,
+            }).then((data) => {
+                if (data[0] > 0) { 
+                    delete req.session.key;
+                    return res.status(200).json({
+                        message: 'Successfully edited post.',
+                    });
+                } else {
+                    return res.status(500).json({
+                        message: 'User not authenticated.'
+                    });
+                }
+            }).catch((err) => {
+                logger.error(errorMsg, err, err.message);
+                return res.status(400).json({
+                    message: errorMsg,
+                    error: err.message,
+                });
+            });
+        } else {
+            return res.status(500).json({
+                message: 'User not authenticated.'
+            })
+        }
+
     }
 
 }
