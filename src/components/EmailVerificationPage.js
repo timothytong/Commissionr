8501310afa81e8377840045d0b5794886b991e78
Timothy Tong <timothy.tong@hotmail.ca>
2018-08-09 16:57:07 +0800

    Add new pages for verification process

---
+++ src/components/EmailVerificationPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class EmailVerificationPage extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+  	render() {
+	    return (
+	        <div>
+				<h1>A verification email has been sent to your email address.</h1>
+			</div>
+	    );
+  	}
+}
---
+++ src/components/UserNotVerifiedPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class UserNotVerifiedPage extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+  	render() {
+	    return (
+	        <div>
+				<h1>User unverified</h1>
+			</div>
+	    );
+  	}
+}
