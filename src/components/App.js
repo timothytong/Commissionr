77a2950aead6c56925476a84ada494b57d2b6975
Timothy Tong <timothy.tong@hotmail.ca>
2018-08-09 16:58:43 +0800

    Add user verification logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import { Switch, Route } from 'react-router-dom';
+
 import LoginPage from './LoginPage';
 import MyHomePage from './MyHomePage';
 import UserHomePage from './UserHomePage';
 import SignUpPage from './SignUpPage';
 import NewPostPage from './NewPostPage';
 import EditProfilePage from './EditProfilePage';
+import EmailVerificationResultPage from './EmailVerificationResultPage';
+import EmailVerificationPage from './EmailVerificationPage';
 import FoundPage from './FoundPage';
+import UserNotVerifiedPage from './UserNotVerifiedPage';
 import React from 'react';
 import axios from 'axios';
 
@@ -15,6 +19,9 @@ export default class App extends React.Component {
 			<Switch>
 		        <Route exact path='/' component={LoginPage} />
 		        <Route exact path='/home' component={MyHomePage} />
+		        <Route exact path='/emailVerificationStarted' component={EmailVerificationPage} />
+		        <Route exact path='/verifyUser' component={EmailVerificationResultPage} />
+		        <Route exact path='/unverifiedUser' component={UserNotVerifiedPage} />
 		        <Route exact path='/updateProfile' component={EditProfilePage} />
 		        <Route exact path='/user/:username' component={UserHomePage} />
 		        <Route exact path='/signup' component={SignUpPage} />
